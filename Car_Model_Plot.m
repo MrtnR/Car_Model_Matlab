1ea526f53db9329e2e706a3e82680d766adf7c42
Martin R. Linares <ie704569@iteso.mx>
2018-11-29 07:32:42 -0600

    Finished the control by blocks
    Pending simulations and results, might wanna have a quick check by the prof.

--- Car_Model_Plot.m
+++ Car_Model_Plot.m
@@ -1,18 +1,19 @@
 function Car_Model_Plot (tspan, x0, Pd)
 
-global Kx Ky Ko Kphi
+global Kx Ky Ko Kphi L
 
 Kx = Pd(1); Ky = Pd(2); Ko = Pd(3); Kphi = Pd(4);
 
+L = 0.4;
 
 
-[t, X] = ode23t()
+[t, X] = ode23t(@Car_Model_Sys, tspan, x0);
 
 end
 
 function dX = Car_Model_Sys(t, X)
 
-global Kx Ky Ko Kphi
+global Kx Ky Ko Kphi L
 
 
 xref = cos(t);
@@ -27,16 +28,18 @@ Oref = atan2(dyref -Ky*ey, dxref - Kx*ex);
 
 W = (dxref - Kx*ey)/(cos(Oref));
 
-Eo = Oref - O;
+Eo = Oref - X(3);
 
-for i=1:length(Oref)
-    dOref
-end
+dOref = diff(Oref);
 
-Phi_ref = atan2()
+Phi_ref = atan2(dOref - Ko*Eo,W)*L;
 
+Ephi = Phi_ref - X(4);
 
+dPhi_ref = diff(Phi_ref);
 
+Wd = dPhi_ref - Kphi*Ephi;
 
+dX = [cos(X(3)); sin(X(3)); tan((X(4)/L); 0]*W + [0; 0; 0; 1]*Wd;
 
 end
